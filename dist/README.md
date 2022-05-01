52e23f62048ae1725769f94c95d834adeb81e957
Derricka S <derrickas728@gmail.com>
2022-05-01 16:23:27 -0400

    add contributors to markdown

---
+++ dist/README.md
@@ -0,0 +1,48 @@
+
+# ll
+
+_liscense_
+
+## Description 
+
+ll
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Contact](#contact)
+
+## Installation
+
+ll
+
+## Usage
+
+ll
+
+## Contributors
+
+* derrtic
+
+* k ds
+
+* fddf
+
+
+
+## Tests
+
+s
+
+## Contact
+
+Any questions, comments, or concern? 
+
+Contact me at: 
+
+[Github](https://github.com/s)
+
+[Email](mailto:s)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,11 +10,31 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license){
+
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license){
+
+  } else {
+    return "";
+  }
+}
+
+function getContributors(contributors) {
+  const markdown = contributors.split(',').map(contributor => {
+    return `* ${contributor.trim()}\n\n`
+  })
+
+  return markdown.join('');
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -45,7 +65,7 @@ ${data.usage}
 
 ## Contributors
 
-$getContributor()}
+${getContributors(data.contributors)}
 
 ## Tests
 
@@ -53,9 +73,12 @@ ${data.tests}
 
 ## Contact
 
-Any questions, comments, or concern? Contact me at: 
+Any questions, comments, or concern? 
+
+Contact me at: 
 
 [Github](https://github.com/${data.github})
+
 [Email](mailto:${data.email})
 `;
 }
